d9a09b6d6abc129b21cd177c16248b0f9e672b24
Akash Goswami <Skyth3r@users.noreply.github.com>
2023-12-29 17:33:01 +0000

    Removed redundant code

--- main.py
+++ main.py
@@ -9,24 +9,13 @@ class Person:
     def __init__(self, name, link):
         self.name = name
         self.link = link
-    
-class VideoItem:
-    def __init__(self, title, people, link, content_type, side_copy, summary, transcript):
-        self.title = title
-        self.people = people
-        self.link = link
-        self.content_type = content_type
-        self.side_copy = side_copy
-        self.summary = summary
-        self.transcript = transcript
 
-class BlogItem:
-    def __init__(self, title, people, link, content_type, article):
+class Item:
+    def __init__(self, title, people, link, content_type):
         self.title = title
         self.people = people
         self.link = link
         self.content_type = content_type
-        self.article = article
 
 if __name__ == '__main__': 
 
@@ -47,8 +36,7 @@ if __name__ == '__main__':
         library_item_link = base_url + library_item.get('href')
         urls_list.append(library_item_link)
 
-    video_items = []
-    blog_items = []
+    items = []
 
     for url in urls_list:
         item_page_response = requests.get(url)
@@ -80,69 +68,28 @@ if __name__ == '__main__':
                 name = person.find('p')
                 person_url = base_url + person['href']
                 people_list.append(Person(name.text, person_url))
-        
-        if content_type == "Video":
-            # side copy
-            aside_div = aside.find('div',  class_='box')
-            side_copy = aside_div.text
-            # summary (if present)
-            if main.find('details'):
-                summary_div = main.find('details').find('div', class_='text')
-                summary = summary_div.text
-            else:
-                summary = ""
-            # transcript (if present)
-            if main.find('article'):
-                article_div = main.find('article')
-                article = article_div.text
-            else:
-                article = ""
-        elif content_type == "Blog":
-            article_div = main.find('article')
-            article = article_div.text
 
-        if content_type == "Video":
-            video_item = VideoItem(
-                title.text,
-                people_list,
-                url,
-                content_type,
-                side_copy,
-                summary,
-                article
-                )
-            video_items.append(video_item)
-        elif content_type == "Blog":
-            blog_item = BlogItem(
-                title.text,
-                people_list,
-                url,
-                content_type,
-                article
-                )
-            blog_items.append(blog_item)
+        # add item to list
+        item = Item(title.text,
+                    people_list,
+                    url,
+                    content_type
+                    )
+        items.append(item)
 
     # create text file
     file = open("data.txt", "a")
 
-    for item in video_items:
+    # add items to text file
+    for item in items:
         md_title = "[" + item.title + "]" + "(" + item.link + ")"
 
-        speakers = []
-        for speaker in item.people:
-            speakers.append("[" + speaker.name.strip() + "]" + "(" + speaker.link + ")")
-    
-        item_string = "| " + md_title + " | " + item.content_type + " | " + ', '.join(speakers) + " | " + '<input type="checkbox" unchecked>' + " |\n" 
-        file.write(item_string)
+        people = []
+        for person in item.people:
+            people.append("[" + person.name.strip() + "]" + "(" + person.link + ")")
+        
+        item_string = "| " + md_title + " | " + item.content_type + " | " + ', '.join(people) + " | " + '<input type="checkbox" unchecked>' + " |\n" 
 
-    for item in blog_items:
-        md_title = "[" + item.title + "]" + "(" + item.link + ")"
-
-        authors = []
-        for author in item.people:
-            authors.append("[" + author.name.strip() + "]" + "(" + author.link + ")")
-
-        item_string = "| " + md_title + " | " + item.content_type + " | " + ', '.join(authors) + " | " + '<input type="checkbox" unchecked>' + " |\n" 
         file.write(item_string)
 
     file.close()
